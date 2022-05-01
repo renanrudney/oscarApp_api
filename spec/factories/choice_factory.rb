cde4ccffe6163008c712115b1482aebb1ab7a99a
Renan Rudney <renanrudney@gmail.com>
2022-05-01 03:54:54 -0300

    test: retrieve choices

--- spec/factories/choice_factory.rb
+++ spec/factories/choice_factory.rb
@@ -2,12 +2,14 @@
 #
 # Table name: choices
 #
-#  id                 :integer          not null, primary key
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  remote_director_id :integer          not null
-#  remote_movie_id    :integer          not null
-#  user_id            :integer
+#  id                   :integer          not null, primary key
+#  remote_director_name :string           not null
+#  remote_movie_name    :string           not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  remote_director_id   :integer          not null
+#  remote_movie_id      :integer          not null
+#  user_id              :integer
 #
 # Indexes
 #
@@ -17,6 +19,8 @@ FactoryBot.define do
   factory :choice do
     remote_movie_id { 1 }
     remote_director_id { 1 }
+    remote_movie_name { Faker::Creature::Dog.name }
+    remote_director_name { Faker::Creature::Dog.name }
     user { User.first }
   end
 end
--- spec/requests/choices_spec.rb
+++ spec/requests/choices_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe "Choices", type: :request do
     let(:user) { create(:user) }
     let(:token) { create(:token, user: user) }
     let(:remote_movie_id) { rand(1..10) }
+    let(:remote_movie_name) { Faker::Creature::Dog.name }
     let(:remote_director_id) { rand(1..10) }
+    let(:remote_director_name) { Faker::Creature::Dog.name }
     let(:headers) {{ login: user.login, token: token.token_number }}
-    let(:params) {{ choice: { remote_movie_id: remote_movie_id, remote_director_id: remote_director_id }}}
+    let(:params) {{ choice: { remote_movie_id: remote_movie_id, remote_director_id: remote_director_id, remote_movie_name: remote_movie_name, remote_director_name: remote_director_name }}}
 
     it "returns 200 if valid choices is provided" do
       post "/choices", headers: headers, params: params
@@ -17,6 +19,8 @@ RSpec.describe "Choices", type: :request do
       data = JSON.parse(response.body, symbolize_names: true)
       expect(data[:choice][:remote_director_id]).to eq(remote_director_id)
       expect(data[:choice][:remote_movie_id]).to eq(remote_movie_id)
+      expect(data[:choice][:remote_director_name]).to eq(remote_director_name)
+      expect(data[:choice][:remote_movie_name]).to eq(remote_movie_name)
     end
 
     it "returns 422 if choice already exists" do
@@ -48,7 +52,7 @@ RSpec.describe "Choices", type: :request do
     end
 
     it "returns 422 if invalid choices is provided" do
-        post "/choices", headers: headers, params: { choice: { remote_movie_id: nil, remote_director_id: nil } }
+        post "/choices", headers: headers, params: { choice: { remote_movie_id: nil, remote_director_id: nil, remote_movie_name: nil,  remote_director_name: nil} }
   
         expect(response).to have_http_status(422)
   
@@ -67,6 +71,46 @@ RSpec.describe "Choices", type: :request do
 
     it "returns 401 if no credentials is provided" do
         post "/choices", params: params
+  
+        expect(response).to have_http_status(401)
+  
+        data = JSON.parse(response.body, symbolize_names: true)
+        expect(data[:error]).to be_truthy
+      end
+  end
+
+  describe "GET /choices" do
+    let(:user) { create(:user) }
+    let(:token) { create(:token, user: user) }
+    let(:headers) {{ login: user.login, token: token.token_number }}
+    let(:choice) {}
+
+    it "returns 200 if valid credentials is provided" do
+      choice = create(:choice, user: user)
+
+      get "/choices", headers: headers
+
+      expect(response).to have_http_status(200)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data[:remote_director_id]).to eq(choice.remote_director_id)
+      expect(data[:remote_movie_id]).to eq(choice.remote_movie_id)
+      expect(data[:remote_director_name]).to eq(choice.remote_director_name)
+      expect(data[:remote_movie_name]).to eq(choice.remote_movie_name)
+      expect(data[:user_id]).to eq(user.id)
+    end
+
+    it "returns 401 if invalid credentials is provided" do
+      get "/choices", headers: { login: Faker::Internet.username, token: rand(0..100) }
+
+      expect(response).to have_http_status(401)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data[:error]).to be_truthy
+    end
+
+    it "returns 401 if no credentials is provided" do
+        get "/choices"
   
         expect(response).to have_http_status(401)
   
